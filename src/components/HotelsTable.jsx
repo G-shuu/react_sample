c8662cb3221d64c4d4b57966e55ed1d55401769f
G-shuu <eng.trinity.24.so.6263@gmail.com>
2018-05-04 04:02:44 +0900

    use rakuten api

--- src/components/HotelsTable.jsx
+++ src/components/HotelsTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import HotelRow from './HotelRow'
+import HotelRow from './HotelRow';
 
 const HotelsTable = ({ hotels }) => (
   <table>
